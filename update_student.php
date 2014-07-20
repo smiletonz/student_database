767c3d7f49ca1fe0dc11aa058557a70cbf347181
Santi Saeheng <tons.st@gmail.com>
2014-07-20 22:44:59 +0700

    added update_student.php file

--- update_student.php
+++ update_student.php
@@ -7,6 +7,12 @@
 	<body>
 		<?php 
 		require_once("database_config.php");
+
+		$selected = mysql_select_db("std");
+  		mysql_query("SET NAMES UTF8");
+  		$num_year = idate('Y') + 543 - 2500 ;
+  		$data = file_get_contents("http://reg.su.ac.th/registrar/learn_time.asp?f_cmd=1&f_studentcode=07".$num_year."*&f_studentstatus=all&f_maxrows=1000", "r");
+  		echo $data;
 		?>
 	</body>
 </html>
